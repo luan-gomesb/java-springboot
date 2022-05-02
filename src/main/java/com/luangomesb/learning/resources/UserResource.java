b587c7efeb60080f2a63b2ef67c70cd24bb5939b
luan-gomesb <luan.gomesb@live.com>
2022-05-02 16:32:51 -0300

    add /user and /user/{id} endpoints

--- src/main/java/com/luangomesb/learning/resources/UserResource.java
+++ src/main/java/com/luangomesb/learning/resources/UserResource.java
@@ -1,20 +1,29 @@
 package com.luangomesb.learning.resources;
 
+import com.luangomesb.learning.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.luangomesb.learning.entities.User;
 
-@RestController
-@RequestMapping(value="/users")
-public class UserResource {
+import java.util.List;
 
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@Autowired
+	UserService userService;
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1,"Luan", "luan.gomesb@live.com","11943824321",null);
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> usersList = userService.findAll();
+
+		return ResponseEntity.ok().body(usersList);
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User user = userService.findById(id);
+		return ResponseEntity.ok().body(user);
+	}
 }
--- src/main/java/com/luangomesb/learning/services/UserService.java
+++ src/main/java/com/luangomesb/learning/services/UserService.java
@@ -1,2 +1,27 @@
-package com.luangomesb.learning.services;public class UserService {
+package com.luangomesb.learning.services;
+
+import com.luangomesb.learning.entities.User;
+import com.luangomesb.learning.repositories.UserReposity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/* Register class as component, so spring can inject with @AutoWired where we need
+We have yet @Repository and @Service to do this, just for semantic
+* */
+@Service
+public class UserService {
+    @Autowired
+    private UserReposity userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+
+    public User findById(Long id){
+        Optional<User> userObj = userRepository.findById(id);
+        return userObj.get();
+    }
 }
