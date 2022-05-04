91be1fba7e9efcabc792735261bcf452b46046e2
luan-gomesb <luan.gomesb@live.com>
2022-05-03 23:27:50 -0300

    Product and OrderItem OneToMane association

--- src/main/java/com/luangomesb/learning/entities/Product.java
+++ src/main/java/com/luangomesb/learning/entities/Product.java
@@ -12,7 +12,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "tb_product")
@@ -31,6 +34,9 @@ public class Product implements Serializable {
   @ManyToMany
   @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
   private Set<Category> categories = new HashSet<>();
+
+  @OneToMany(mappedBy = "id.product")
+  private Set<OrderItem> items = new HashSet<>();
 
   public Product() {
   }
@@ -85,6 +91,13 @@ public class Product implements Serializable {
 
   public Set<Category> getCategories() {
     return categories;
+  }
+
+  @JsonIgnore
+  public Set<Order> getItems() {
+    Set<Order> orders = new HashSet<>();
+    items.stream().forEach((OrderItem item) -> orders.add(item.getOrder()));
+    return orders;
   }
 
   @Override
