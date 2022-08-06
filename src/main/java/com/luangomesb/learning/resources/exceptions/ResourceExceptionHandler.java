d68dbc38edf82c9f3d46989b8e4ba567af7d3bc3
luan-gomesb <luan.gomesb@live.com>
2022-08-06 20:14:36 -0300

    correct

--- src/main/java/com/luangomesb/learning/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/luangomesb/learning/resources/exceptions/ResourceExceptionHandler.java
@@ -2,7 +2,6 @@ package com.luangomesb.learning.resources.exceptions;
 
 import javax.servlet.http.HttpServletRequest;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -12,23 +11,24 @@ import java.time.Instant;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
-    //confgure method to intercept the error
+    // confgure method to intercept the error
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<StandardException> resourceNotFound(ResourceNotFoundException e,HttpServletRequest request){
-	    String error = "Resource Not Found!";
+    public ResponseEntity<StandardException> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource Not Found!";
         HttpStatus status = HttpStatus.NOT_FOUND;
-        StandardException err = new StandardException(Instant.now(), status.value(),error,e.getMessage(), request.getRequestURI());
+        StandardException err = new StandardException(Instant.now(), status.value(), error, e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 
     @ExceptionHandler
-    public ResponseEntity<StandardException> database(DatabaseException e, HttpServletRequest request){
+    public ResponseEntity<StandardException> database(DatabaseException e, HttpServletRequest request) {
         String error = "Database Exception!";
         HttpStatus status = HttpStatus.BAD_REQUEST;
-        StandardException err = new StandardException(Instant.now(), status.value(),error,e.getMessage(), request.getRequestURI());
+        StandardException err = new StandardException(Instant.now(), status.value(), error, e.getMessage(),
+                request.getRequestURI());
         return ResponseEntity.status(status).body(err);
 
     }
-
 
 }
